d13ce040185f3e68b7bd3230fc0b1f409bc3afea
forest <y.morisaki@mac.com>
2022-06-22 14:36:14 +0900

    微調整

--- python_crawler_1.py
+++ python_crawler_1.py
@@ -3,9 +3,9 @@ import lxml.html
 
 response = requests.get( 'https://gihyo.jp/dp' )
 html = lxml.html.fromstring( response.text )
-html.make_links_absolute(response.url)
+html.make_links_absolute( response.url )
 
 for a in html.cssselect( '#listBook > li > a[itemprop="url"]' ):
-    url = a.get('href')
+    url = a.get( 'href' )
     print( url )
 
